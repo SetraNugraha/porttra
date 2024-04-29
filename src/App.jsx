381b028c77a6b1bc4778a232aa079b7d971d028b
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-04-29 23:43:31 +0700

    fix cursor navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Swal from 'sweetalert2'
 const NavMenu = ({ title, path, key }) => {
   return (
     <>
-      <li key={key} className="hover:bg-slate-200 hover:rounded-lg py-2">
-        <Link to={path} spy={true} smooth={true} offset={-80} duration={1000} className="px-2 font-semibold text-slate-700">
+      <li key={key} className="hover:bg-slate-200 hover:rounded-lg cursor-pointer py-2">
+        <Link to={path} spy={true} smooth={true} offset={-80} duration={1200} className="px-2 font-semibold text-slate-700 cursor-pointer">
           {title}
         </Link>
       </li>
@@ -292,9 +292,9 @@ export default function App() {
       <nav className="fixed bg-white h-[50px] w-full flex justify-between items-center px-5 drop-shadow-xl z-10 || xl:h-[60px]">
         <div className=" ||  xl:w-[60%] xl:mx-auto xl:justify-between xl:items-center xl:flex ">
           {/* Title */}
-          <div className="">
+          <div>
             <Link to="hero" spy={true} smooth={true} offset={-70} duration={1000}>
-              <h1 className="text-lg font-semibold || xl:text-2xl xl:font-bold">Porttra</h1>
+              <h1 className="text-lg font-semibold cursor-pointer || xl:text-2xl xl:font-bold">Porttra</h1>
             </Link>
           </div>
 
