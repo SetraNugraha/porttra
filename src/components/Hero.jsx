ee5bfdbd6dba4761d0da07dcc1d549e330cb7b9f
Setra Nugraha Putra Suma <setranugraha75@gmail.com>
2024-09-28 23:47:10 +0700

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,10 @@
-import { SocialMediaLink } from "../elements/SocialMediaLink"
 import { IoMdCloudDownload } from "react-icons/io"
 import { PiCertificateFill } from "react-icons/pi"
+import { FaFilePdf } from "react-icons/fa";
+import { PiFilePdfFill } from "react-icons/pi";
+import { MdPictureAsPdf } from "react-icons/md";
+import { TbFileTypePdf } from "react-icons/tb";
+
 
 export default function Hero() {
   return (
@@ -12,36 +16,36 @@ export default function Hero() {
 
       <div className="flex flex-col justify-center items-center mt-5 || xl:w-1/2 xl:items-start">
         {/* Greeting */}
-        <div className="flex flex-col items-center || xl:items-start">
+        <div className="flex flex-col">
           <h3 className="text-xs text-secondary tracking-[6px] flex items-center before:w-[30px] before:h-[1px] before:bg-secondary before:mr-3 || xl:text-lg xl:font-semibold">HELLO, IT&apos;S ME</h3>
           {/* Name */}
           <h1 className="text-2xl font-bold text-slate-700 || xl:text-3xl">
             Setra Nugraha <span className="text-stone-500">Putra Suma</span>
           </h1>
+          <div className="flex gap-x-3 mt-3 self-start xl:mt-5">
+            <a
+              href="resume-setra.pdf"
+              target="_blank"
+              className="text-[14px] font-semibold text-white bg-stone-500 py-1 px-3 rounded-lg flex gap-x-2 items-center hover:bg-white hover:outline-none hover:ring-2 hover:ring-stone-500 hover:text-stone-500 duration-300 shadow-lg shadow-gray-300 || xl:text-lg">
+              <TbFileTypePdf size={22} />
+              <span className="tracking-wider">Resume</span>
+            </a>
+            <a
+              href="sertifikasi-setra.pdf"
+              target="_blank"
+              className="text-[14px] font-semibold text-white bg-[#455a64] py-1 px-3 rounded-lg flex gap-x-2 items-center hover:bg-white hover:outline-none hover:ring-2 hover:ring-[#455a64] hover:text-[#455a64] duration-300 shadow-lg shadow-gray-300 || xl:text-lg">
+              <PiCertificateFill size={22} />
+              <span className="tracking-wider">Certification</span>
+            </a>
+           
+          </div>
         </div>
 
         {/* desc */}
-        <div className="mt-2 w-full flex flex-col gap-y-3 py-1 px-6 bsg-red-200 xl:mx-0 xl:px-0">
-          <div className="flex flex-col gap-y-5 items-start">
-            <div className="flex items-center gap-x-3">
-              <a
-                href="resume-setra.pdf"
-                target="_blank"
-                className="text-[14px] font-semibold text-white bg-stone-500 py-1 px-3 rounded-lg flex gap-x-2 items-center hover:bg-white hover:outline-none hover:ring-2 hover:ring-stone-500 hover:text-stone-500 duration-300 shadow-lg shadow-gray-300 || xl:text-lg">
-                <IoMdCloudDownload className="w-[20px] h-[20px]" />
-                <span className="tracking-wider">Resume</span>
-              </a>
-              <a
-                href="sertifikasi-setra.pdf"
-                target="_blank"
-                className="text-[14px] font-semibold text-white bg-[#455a64] py-1 px-3 rounded-lg flex gap-x-2 items-center hover:bg-white hover:outline-none hover:ring-2 hover:ring-[#455a64] hover:text-[#455a64] duration-300 shadow-lg shadow-gray-300 || xl:text-lg">
-                <PiCertificateFill className="w-[20px] h-[20px]" />
-                <span className="tracking-wider">Certification</span>
-              </a>
-            </div>
-            <div className="ml-1">
-              <SocialMediaLink />
-            </div>
+        <div className="mt-2 w-full flex flex-col items-start px-11 gap-y-3 py-1 xl:items-start xl:mx-0 xl:px-0">
+          <div className="flex flex-col gap-y-5 items-start xl:items-start">
+            <div className="flex items-center gap-x-3"></div>
+            
           </div>
         </div>
       </div>
